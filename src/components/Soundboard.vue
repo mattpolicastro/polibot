97237462d69fec0f126b81aa22064d670b4b786a
Matt Policastro <matt@mattpolicastro.com>
2020-10-09 23:27:26 -0500

    More minor copy/linebreak edits

--- src/components/Soundboard.vue
+++ src/components/Soundboard.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="container">
     <h3>soundboard</h3>
-    <i>polibot is still collecting files; in the meantime, enjoy some dings and/or dongs.</i>
-    <p></p>
+    <i>polibot is still collecting clips; in the meantime,</i>
+    <br>
+    <i>enjoy some dings and/or dongs.</i>
+    <br>
     <i>(:sir:)</i>
     <!-- <i>. keep an eye on the <a href="https://github.com/mattpolicastro/polibot" target="_blank" rel="noopener">github repo</a>.</i> -->
     <div class="layer" id="soundboard">
