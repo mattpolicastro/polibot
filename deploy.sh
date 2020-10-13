beacf4bd50a215a60fc0598860c8645470841e1b
Matt Policastro <matt@mattpolicastro.com>
2020-10-13 00:18:56 -0500

    Updated deploy.sh for new project repo

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mattpolicastro/clearhead_farewell.git master:gh-pages
+git push -f git@github.com:mattpolicastro/soundblasterblaster.git master:gh-pages
 
 cd -
